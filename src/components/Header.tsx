09588bf74d07a9eaf3b6a404734db4e74761e3dd
Terence Yebuah <terence.yebuah@gmail.com>
2025-07-02 14:09:02 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-white flex justify-between items-center p-10 pb-5">
-            <h1 className="text-5xl font-semibold text-black">PUT ME <br/> ON</h1>
+        <header className="sticky top-0 z-50 bg-white flex justify-between items-center px-6 pt-6 pb-4">
+        <h1 className="text-5xl font-semibold text-black">PUT ME <br/> ON</h1>
             <a href="/recs" className="text-black font-bold hover:underline text-2xl">Your <br/> Recs</a>
         </header>
     );
