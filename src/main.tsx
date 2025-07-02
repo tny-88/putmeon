033883088c3d0cb0b016c6f64790eb7f09a9a77f
Terence Yebuah <terence.yebuah@gmail.com>
2025-07-02 09:15:51 +0000

    Added Speed Insights analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Toaster } from 'sonner';
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
+        <SpeedInsights />
         <Toaster richColors position="bottom-center" />
     </React.StrictMode>,
 );
