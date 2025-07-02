0cce1a67070dddb52d7427ecaa5890ea1d288df8
Terence Yebuah <terence.yebuah@gmail.com>
2025-07-02 09:22:33 +0000

    Corrected the speed insights to be for react

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Toaster } from 'sonner';
-import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
